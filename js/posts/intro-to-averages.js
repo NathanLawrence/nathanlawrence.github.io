c149ec3a206159e5aa369ed08e4e22d403fdd6f3
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2016-05-01 23:55:20 -0500

    Cleaned up my javascript and added some better random number work.

--- js/posts/intro-to-averages.js
+++ js/posts/intro-to-averages.js
@@ -1,70 +1,78 @@
-var bardata = [Math.random(),Math.random(),Math.random(),Math.random(),Math.random(),Math.random(),Math.random(),Math.random(),Math.random()];
-var height = 300,
-	width = 750;
-	
-var barOffset = .1;
-
-var paddingTop = 40,
-	paddingRight = 55,
-	paddingBottom = 30,
-	paddingLeft = 55;
-
-var yScale = d3.scale.linear()
-	.domain([0,d3.max(bardata)])
-	.range([0, (height - paddingTop - paddingBottom)]);
-
-var xScale = d3.scale.ordinal()
-	.domain(d3.range(0,bardata.length))
-	.rangeRoundBands([0,width - paddingRight - paddingLeft],barOffset,0)
-
-var barWidth = (width - paddingLeft - paddingRight - 2*barOffset)/bardata.length;
-
-d3.select('#main-chart').append('svg')
-	.attr('width',width)
-	.attr('height',height)
-	.selectAll('rect')
-	.data(bardata)
-	.enter().append('rect')
-		.style('fill', '#ddd')
-		.attr('width', xScale.rangeBand())
-		.attr('height', function(d){
-			return yScale(d);
-		})
-		.attr('x', function(d,i){
-			return xScale(i) +paddingLeft;
-		})
-		.attr('y', function(d){
-			return height - yScale(d) - paddingTop + paddingBottom;
-		});
-
-
-
 $(document).ready(function() {
-			// Grab the initial top offset of the chart.
-		   	var stickyChartTop = $('#main-chart').offset().top;
-		   	var stickyChartLeft = $('#main-chart').offset().left;
-		   	
-		   	// This function will decide if the navigation bar should have "sticky" class or not.
-		   	var stickyChart = function(){
-			    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
-			         
-			    // If we've scrolled the chart off the page, even the tiniest bit, change its position to fixed to stick to top.
-			    // Otherwise, change it back to relative
-			    if (scrollTop > stickyChartTop) { 
-			        $('#main-chart').addClass('sticky');
-			        $('#main-chart').css('left',stickyChartLeft - 10);
-			        $('#main-chart').css('background-color',$('body').css('background-color'));
-			        $('#main-chart').css('border-color',$('body').css('background-color'));
-			        $('.before-main-chart').css('padding-bottom',$('#main-chart').height());
-			    } else {
-			        $('#main-chart').removeClass('sticky'); 
-			        $('.before-main-chart').css('padding-bottom',0);   
-			    }
-			};
 
-			stickyChart();
-			// and run it again every time you scroll
-			$(window).scroll(function() {
-				stickyChart();
+	//STARTER D3 CODE BEGINS HERE
+
+	var bardata = [Math.floor(Math.random() * 9) + 1, Math.floor(Math.random() * 9) + 1, Math.floor(Math.random() * 9) + 1, Math.floor(Math.random() * 9) + 1, Math.floor(Math.random() * 9) + 1, Math.floor(Math.random() * 9) + 1, Math.floor(Math.random() * 9) + 1, Math.floor(Math.random() * 9) + 1, Math.floor(Math.random() * 9) + 1];
+	var height = 400,
+		width = $('#main-chart').width();
+		
+	var barOffset = .1;
+
+	var paddingTop = 40,
+		paddingRight = 55,
+		paddingBottom = 30,
+		paddingLeft = 55;
+
+	var yScale = d3.scale.linear()
+		.domain([0,d3.max(bardata)])
+		.range([0, (height - paddingTop - paddingBottom)]);
+
+	var xScale = d3.scale.ordinal()
+		.domain(d3.range(0,bardata.length))
+		.rangeRoundBands([0,width - paddingRight - paddingLeft],barOffset,0)
+
+	var barWidth = (width - paddingLeft - paddingRight - 2*barOffset)/bardata.length;
+
+	d3.select('#main-chart').append('svg')
+		.attr('width',width)
+		.attr('height',height)
+		.selectAll('rect')
+		.data(bardata)
+		.enter().append('rect')
+			.style('fill', '#ddd')
+			.attr('width', xScale.rangeBand())
+			.attr('height', function(d){
+				return yScale(d);
+			})
+			.attr('x', function(d,i){
+				return xScale(i) +paddingLeft;
+			})
+			.attr('y', function(d){
+				return height - yScale(d) - paddingTop + paddingBottom;
 			});
-		});
+
+	// STARTER D3 CODE ENDS HERE
+
+
+	// STARTER JQUERY CODE STARTS HERE
+	$('.set-lowest').html(Math.min.apply(Math, bardata));
+	$('.set-highest').html(Math.max.apply(Math, bardata));
+
+	// Grab the initial top offset of the chart.
+   	var stickyChartTop = $('#main-chart').offset().top;
+   	var stickyChartLeft = $('#main-chart').offset().left;
+   	
+   	// This function will decide if the navigation bar should have "sticky" class or not.
+   	var stickyChart = function(){
+	    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+	         
+	    // If we've scrolled the chart off the page, even the tiniest bit, change its position to fixed to stick to top.
+	    // Otherwise, change it back to relative
+	    if (scrollTop > stickyChartTop - 3) { //These three pixels of "room" help prevent a bouncing effect where the JS hasn't caught up to the page.
+	        $('#main-chart').addClass('sticky');
+	        $('#main-chart').css('left',stickyChartLeft - 10);
+	        $('#main-chart').css('background-color',$('body').css('background-color'));
+	        $('#main-chart').css('border-color',$('body').css('background-color'));
+	        $('.before-main-chart').css('padding-bottom',$('#main-chart').height());
+	    } else {
+	        $('#main-chart').removeClass('sticky'); 
+	        $('.before-main-chart').css('padding-bottom',0);   
+	    }
+	};
+
+	stickyChart();
+	// and run it again every time you scroll
+	$(window).scroll(function() {
+		stickyChart();
+	});
+});
